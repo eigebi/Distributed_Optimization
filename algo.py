f6b01e01f01d931667c5cf148a9cb861345fa474
Shuo Chen <1528674004@qq.com>
2024-08-02 11:18:18 +0100

    add .gitignore

--- algo.py
+++ algo.py
@@ -1,2 +1,10 @@
 import torch
 import numpy as np
+
+class dADMM:
+    def __init__(self, rho, alpha, beta, gamma, max_iter):
+        self.rho = rho
+        self.alpha = alpha
+        self.beta = beta
+        self.gamma = gamma
+        self.max_iter = max_iter
