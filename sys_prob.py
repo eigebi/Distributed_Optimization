311c4c271c2c8cf8393d2620d6cf5fc6df928eb4
Shuo Chen <1528674004@qq.com>
2024-08-07 23:39:32 +0100

    test algorithm v 0.1

--- sys_prob.py
+++ sys_prob.py
@@ -11,7 +11,7 @@ class BaseProblem:
         self.func = func
         #this ID is only used to call the global variable
         self.varID = np.array(varID)
-        self.gamma = np.abs(np.random.randn(len(varID)))
+        self.gamma = np.random.randn(len(varID))
         self.is_min = is_min
         self.x_next = []
     def __call__(self, x):
@@ -25,7 +25,7 @@ class BaseProblem:
         pass
 
     def optimize(self,x_global):
-        rho = 1
+        rho = 10
         x_i = x_global[self.varID]
         f_i = lambda x: self.func(x)+self.gamma@(x-x_i)+rho/2*np.linalg.norm(x-x_i)**2
         if self.is_min:
@@ -56,7 +56,8 @@ if __name__ == '__main__':
     f_s = []
     for _ in range(5):
             temp = np.random.randn(3,3)
-            temp = temp @ temp.T
+            #temp = temp @ temp.T
+
             f_s.append(lambda x: x @ temp @ x)
         #generate 3 integers from (1,5) without repetition
         #np.random.choice(5,3,replace=False)
@@ -77,10 +78,12 @@ if __name__ == '__main__':
     # global lambda
     y = np.abs(np.random.randn(11))
     #gamma = np.abs(np.random.randn(11)) this gamma is local
+    for i in range(11):
+        p.r[i].x_next = y[i]
 
 
-    T = 100
-    rho = 1
+    T = 500
+    rho = 100
 
 
     # need an index table to connect z to all the local variables, which is inverted to the varID
@@ -109,15 +112,15 @@ if __name__ == '__main__':
         for i in range(5):
             # to be wrapped into optimize()
             z_i= z[p.obj[i].varID]
-            f_i = lambda x: p.obj[i].func(x)+p.obj[i].gamma@(x-z_i)+rho/2*np.linalg.norm(x-z_i)**2
+            f_i = lambda x: p.obj[i].func(x)+p.obj[i].gamma@(x-z_i)+rho/2*np.linalg.norm(x-z_i)**4
             x_i = minimize(f_i,np.zeros(len(p.obj[i].varID))).x
             p.obj[i].x_next = x_i
             #the square norm of x-z
         for i in range(11):
             # to be wrapped into optimize()
             z_i= z[p.con[i].varID]
-            f_i = lambda x: y[i]*p.con[i].func(x)+p.con[i].gamma@(x-z_i)+rho/2*np.linalg.norm(x-z_i)**2
-            x_i = minimize(f_i,np.zeros(len(p.r[i].varID))).x
+            f_i = lambda x: p.r[i].x_next*p.con[i].func(x)+p.con[i].gamma@(x-z_i)+rho/2*np.linalg.norm(x-z_i)**4
+            x_i = minimize(f_i,np.zeros(len(p.con[i].varID))).x
             p.con[i].x_next = x_i
             #the square norm of x-y
             #it is better to store the con_func value to serve the next step update
@@ -126,7 +129,7 @@ if __name__ == '__main__':
         for i in range(11):
             # to be wrapped into optimize()
             y_i= y[p.r[i].varID]
-            f_i = lambda r: -r*p.con[i](p.con[i].x_next) + p.r[i].gamma@(r-y_i)+rho/2*np.linalg.norm(r-y_i)**2
+            f_i = lambda r: -r*p.con[i](p.con[i].x_next) + p.r[i].gamma@(r-y_i)+rho/2*np.linalg.norm(r-y_i)**4
             constraint = LinearConstraint(np.eye(1),0)
             r_i = minimize(f_i,np.zeros(len(p.r[i].varID)),constraints=constraint).x
             p.r[i].x_next = r_i
@@ -137,11 +140,26 @@ if __name__ == '__main__':
         #then update the global variables
         #derive the index together is better, but here we call the index again (to be optimized)
         for i in range(len(z)):
-            z[i]=(sum(z_obj_id[i]))
+            z[i] = sum([p.obj[z_obj_id[i][k][0]].x_next[z_obj_id[i][k][1]] for k in range(len(z_obj_id[i]))])+sum([p.con[z_con_id[i][k][0]].x_next[z_con_id[i][k][1]] for k in range(len(z_con_id[i]))])
+            z[i] += sum([p.obj[z_obj_id[i][k][0]].gamma[z_obj_id[i][k][1]] for k in range(len(z_obj_id[i]))])/rho+sum([p.con[z_con_id[i][k][0]].gamma[z_con_id[i][k][1]] for k in range(len(z_con_id[i]))])/rho
+            z[i] = z[i]/(len(z_obj_id[i])+len(z_con_id[i]))
 
         for i in range(len(y)):
-            y[i] = 1/11*sum([p.r[j].x_next for j in range(11)])
+            y[i] = p.r[i].x_next + 1/rho*y[i]
 
-    a = []
+        for i in range(5):
+            p.obj[i].gamma = p.obj[i].gamma + rho*(p.obj[i].x_next-z[p.obj[i].varID])
+        for i in range(11):
+            p.con[i].gamma = p.con[i].gamma + rho*(p.con[i].x_next-z[p.con[i].varID])
+        for i in range(11):
+            p.r[i].gamma = p.r[i].gamma + rho*(p.r[i].x_next-y[p.r[i].varID])
+        output = sum([p.obj[k](p.obj[k].x_next) for k in range(5)])
+        print(output)
+        print('\n')
 
-       
+    pass
+    #compare with fmincon
+    f = lambda x: sum([p.obj[k](x[p.obj[k].varID]) for k in range(5)])   
+
+    x0 = np.random.randn(5)
+    constraint = LinearConstraint(np.eye(5),0,30)
