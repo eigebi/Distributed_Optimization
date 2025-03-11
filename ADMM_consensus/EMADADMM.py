76327bf041d1272209f2247ba470830abbcb07e1
Shuo Chen <1528674004@qq.com>
2025-03-11 19:35:39 +0000

    EMADADMM routing

--- ADMM_consensus/EMADADMM.py
+++ ADMM_consensus/EMADADMM.py
@@ -6,17 +6,83 @@ from scipy.linalg import block_diag
 np.random.seed(10000)
 
 
+
+
+
+class AP:
+    def __init__(self, num_agent):
+        self.num_agent = num_agent
+        location = np.random.randint(0, 100, (num_agent, 2))
+        # each sub network connets to at most 2 other sub networks
+        distance = np.zeros((num_agent, num_agent))
+        for i in range(num_agent):
+            for j in range(i+1, num_agent):
+                distance[i, j] = np.linalg.norm(location[i] - location[j])
+                distance[j, i] = distance[i, j]
+        neighbor = np.zeros((num_agent, num_agent), dtype=np.int32)
+        for i in range(num_agent):
+            index = np.argsort(distance[i])
+            neighbor[i, index[1]] = 1
+            neighbor[i, index[2]] = 1
+        self.neighbor = neighbor
+        self.location = location
+        # plot the network  topology
+        import matplotlib.pyplot as plt
+        plt.scatter(location[:, 0], location[:, 1])
+        for i in range(num_agent):
+            for j in range(num_agent):
+                if neighbor[i, j] == 1:
+                    plt.plot([location[i, 0], location[j, 0]], [location[i, 1], location[j, 1]])
+        plt.show()
+        
+        # assign edges to nodes
+        
+class local_AP:
+    def __init__(self,num_var):
+        alpha = np.random.uniform(-2, -1, num_var)
+        beta = np.random.uniform(1, 9, num_var)
+        #ub = np.random.randint(5, 20, num_var)
+        self.ub = 2*beta    # symetric
+        self.obj = lambda x: 1/(1+np.exp(alpha*(x-beta)))
+        self.jac = lambda x: -(alpha*np.exp(-alpha*(x-beta)))/(1+np.exp(-alpha*(x-beta)))**2
+        self.var_index = None
+        #but remember this is a utilization function, needs to be maximized, or minimize its negative
+class AP_problem:
+    def __init__(self, num_var, num_agent, num_con):
+        self.num_var = num_var # list
+        self.num_agent = num_agent # 2
+        self.num_con = num_con
+        A = []
+        for i in range(self.num_agent):
+            A.append(np.random.randint(0, 2, (self.num_con, self.num_var[i])))
+        self.A = np.concatenate(A, axis=1)
+        self.C = np.random.randint(10,30,self.num_con)
+        temp = np.add.reduceat(self.A, [0,self.num_var[0]], axis=1)
+        self.con_assignment = np.argsort(temp)[:,-1]
+        self.local_probs = [local_AP(self.num_var[i]) for i in range(num_agent)]
+        self.derive_local_id()
+        pass
+    def derive_local_id(self):
+        for i in range(self.num_agent):
+            index_i = np.zeros(sum(self.num_var),dtype=np.int32)
+            index_i[np.sum(self.num_var[0:i],dtype=np.int32):np.sum(self.num_var[0:i+1],dtype=np.int32)] = 1
+            index_i = np.where((index_i + np.sum(self.A[self.con_assignment==i],axis=0))>0)[0]
+            self.local_probs[i].var_index = index_i
+
 class local_problem:
     def __init__(self,num_var,prob_arg):
         self.prob_arg = prob_arg
-        diag = np.random.randint(5,20,num_var)
+        diag = np.random.randint(5,10,num_var)
         mu = np.random.uniform(0.0001,10,num_var)
         self.L_fi = np.max(np.abs(mu/diag))
+        self.L_fi = 10
         
         temp = np.diag(mu)
         self.obj = lambda x: (x/diag-1)@ temp@(x/diag-1) - x@(1/diag)
-        self.jac = lambda x: 2 * temp@(x/diag-1) - 1/diag
-        self.ub = np.random.randint(5,21,num_var)
+        self.jac = lambda x: 2/diag * temp@(x/diag-1)- 1/diag
+        self.obj = lambda x: np.sum(-1/(1+np.exp(mu*(x-diag))))
+        self.jac = lambda x: +(mu*np.exp(-mu*(x-diag)))/(1+np.exp(-mu*(x-diag)))**2
+        self.ub = np.random.randint(10,20,num_var)
         self.var_index = None
 
 
@@ -28,13 +94,15 @@ class problem_generator:
         self.num_con = num_con
         self. arg = arg
         self.A = np.random.randint(0, 2, (self.num_con, self.num_var))
-        self.C = np.random.randint(2,5,self.num_con)
+        self.C = np.random.randint(5,10,self.num_con)
         
         self.num_var_local = self.num_var//self.num_agent
         self.num_var_local_ = self.num_var_local + self.num_var%self.num_agent
         temp = np.add.reduceat(self.A, np.arange(0, self.num_var, self.num_var_local),axis=1)
-        temp[-2] += temp[-1]
-        self.con_assignment = np.argsort(temp[:,:-1])[:,-1]
+        if self.num_var%self.num_agent!=0:
+            temp[:,-2] += temp[:,-1]
+            temp = temp[:,:-1]
+        self.con_assignment = np.argsort(temp)[:,-1]
         self.local_probs = [local_problem(self.num_var_local,arg) for _ in range(num_agent-1)]
         self.local_probs.append(local_problem(self.num_var_local_, arg))
         self.derive_local_id()
@@ -42,12 +110,12 @@ class problem_generator:
 
     def derive_local_id(self):
         for i in range(self.num_agent):
-            index_i = np.zeros(self.num_var)
+            index_i = np.zeros(self.num_var,dtype=np.int32)
             if i < self.num_agent - 1:
                 index_i[i*self.num_var_local:(i+1)*self.num_var_local] = 1
             else:
                 index_i[i*self.num_var_local:] = 1
-            index_i = np.where(index_i + np.sum(self.A[self.con_assignment==i],axis=0)>0)[0]
+            index_i = np.where((index_i + np.sum(self.A[self.con_assignment==i],axis=0))>0)[0]
             self.local_probs[i].var_index = index_i
 
     def gradient_x(self, agent_id, x_i):
@@ -71,9 +139,25 @@ class problem_generator:
                 indices = np.arange(i * self.num_var_local, self.num_var)
 
             obj.append(self.local_probs[i].obj(z[indices]))
-        return np.sum(obj)  
+        return np.sum(obj) 
+    
+    def opt_solution(self):
+        for i in range(self.num_agent):
+            global_ub = np.concatenate([self.local_probs[j].ub for j in range(self.num_agent)])
+        con1 = LinearConstraint(self.A, 0, self.C)
+        con2 = LinearConstraint(np.eye(self.num_var), 0, global_ub)
+        x0 = np.random.randn(self.num_var)
+        res = minimize(self.obj, x0,constraints=[con1, con2])
+        return res
 
 if __name__ == '__main__':
+    #network = AP(10)
+
+    AP_problems = AP_problem([20,15], 2, 5)
+    pass
+
+
+
     class prob_arg:
         sigma_1 = 1
         mu_1 = 0
@@ -83,12 +167,15 @@ if __name__ == '__main__':
         total_resource = 26
         overall_ub = 5
 
-    N=2
+    N=1
     num_var = 5
-    num_con = 2
+    num_con =5
     problems = problem_generator(num_var, N, num_con, prob_arg)
     #problems.gradient_x(0,np.random.randn(1000//16),np.random.randn(20))
-    ub = np.concat([problems.local_probs[i].ub for i in range(N)])
+    ub = np.concatenate([problems.local_probs[i].ub for i in range(N)])
+    opt = problems.opt_solution()
+    g_t = opt.fun
+
     M_g = 1
     
     sigma = 500
@@ -96,15 +183,19 @@ if __name__ == '__main__':
     L_f = [problems.local_probs[i].L_fi for i in range(N)]
     L_f = np.max(L_f)
     tau = 5
-    rho = (N*tau*L_f)**2+np.sqrt((N*tau*L_f)**4+7*N*N*tau*tau*L_f*L_f*L_f)
-    rho = 10
+    #rho = (N*tau*L_f)**2+np.sqrt((N*tau*L_f)**4+7*N*N*tau*tau*L_f*L_f*L_f)
+    rho = 5
     theta = rho*rho/(15*M_g*M_g*(2*rho+7*L_f))
-    beta = (N*tau)**2*L_f/2
+    
 
 
+    obj_iter = []
+    obj_t = []
 
     base_p = np.random.uniform(0.9, 1, N)
+    #base_p = np.ones(N)
     z = 20*np.random.randn(num_var)
+    z = np.clip(z, 0, ub)
     z_graph = np.zeros((num_var, N),dtype=np.int32)
     for i in range(num_var):
         for n in range(N):
@@ -113,7 +204,6 @@ if __name__ == '__main__':
     dx = np.zeros((N,num_var), dtype=np.float32)
     for i in range(N):
         dx[i] = problems.gradient_x(i, z)
-    #latest_x = np.zeros((N,num_var), dtype=np.float32)
     latest_x = np.tile(z, (N, 1))
     gamma = np.zeros((N,num_var), dtype=np.float32)
     sample = np.zeros(N, dtype=np.int32)
@@ -121,32 +211,42 @@ if __name__ == '__main__':
     delay = np.zeros(N, dtype=np.int32)
     id_not_done = np.ones(N, dtype=np.int32)
     k = 1.0 #time step for global update
-    for t in range(500):
+    for t in range(20000):
+        obj_t.append(problems.obj(z))
         for i in range(N):
             sample[i] = np.random.choice([0,1], p=[base_p[i], 1 - base_p[i]])
         id_not_done = id_not_done * sample
-        delay[id_not_done==1] += 1
-        if np.max(delay[id_not_done])>=tau:
+        delay[np.where(id_not_done==1)] += 1
+        if np.any(id_not_done==1) and np.max(delay[np.where(id_not_done==1)])>=tau:
             continue
         # local update
         for i in np.where(id_not_done==0)[0]:
-            delta = dx[i] + r[problems.con_assignment == i] @ problems.A[problems.con_assignment == i]
+            delta = dx[i] + (r[:,np.newaxis]*problems.A)[problems.con_assignment == i].sum(axis=0)
             latest_x[i] = z - (delta + gamma[i]) / rho
             latest_x[i] = np.clip(latest_x[i], 0, ub)
             gamma[i] = gamma[i] + rho * (latest_x[i] - z)
         # global update
-        eta = 1/(rho*np.pow(k,1/4))
+        eta = 1/(rho*np.power(k,1/4))
+        beta = 0.2*(N*tau)**2*L_f/2 + theta#+4/(theta*eta*eta)
 
         for i in range(num_var):
             _id = z_graph[i]
             z[i] = (np.sum(gamma[_id==1,i])+rho*np.sum(latest_x[_id==1,i])+beta*z[i])/(beta+np.sum(_id)*rho)
-        r = np.maximum((problems.A @ z - problems.C + r ) / (eta*theta + 1), 0)
+        z = np.clip(z, 0, ub)
+        r = np.maximum((theta*(problems.A @ z - problems.C) + r ) / (eta*theta + 1), 0)
         k += 1
         for i in np.where(id_not_done==0)[0]:
-            dx[i] = problems.gradient_x(i==0, z)
+            dx[i] = problems.gradient_x(i, z)
             delay[i] = 0
         id_not_done = np.ones(N, dtype=np.int32)
         obj = problems.obj(z)
-        print(obj, problems.A @ z - problems.C,r,z,k)
+        obj_iter.append(obj)
+        print(obj,r,k)
+        #np.save('obj_iter_tau'+str(tau)+'g_t'+str(g_t)+".npy", obj_iter)
+        #np.save('obj_t_tau'+str(tau)+'g_t'+str(g_t)+".npy", obj_t)
+        #np.save('obj_iter_N'+str(N)+'g_t'+str(g_t)+".npy", obj_iter)
+        #np.save('obj_t_N'+str(N)+'g_t'+str(g_t)+".npy", obj_t)
+
+    pass
 
                 
---
+++ ADMM_consensus/plot.py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+obj_iter = [np.load('obj_iter_tau5g_t181.69592941946954.npy')\
+            ,np.load('obj_iter_tau4g_t181.69592941946954.npy')\
+            ,np.load('obj_iter_tau3g_t181.69592941946954.npy')\
+            ,np.load('obj_iter_tau2g_t181.69592941946954.npy')\
+            ,np.load('obj_iter_tau1g_t181.69592941946954.npy')]
+obj_t = [np.load('obj_t_tau5g_t181.69592941946954.npy')\
+            ,np.load('obj_t_tau4g_t181.69592941946954.npy')\
+            ,np.load('obj_t_tau3g_t181.69592941946954.npy')\
+            ,np.load('obj_t_tau2g_t181.69592941946954.npy')\
+            ,np.load('obj_t_tau1g_t181.69592941946954.npy')]
+plt.figure(1)
+colors = ['b', 'g', 'r', 'c', 'y']
+for i in range(5):
+    plt.plot(1-np.abs(obj_iter[i]-181.696)/181.696+0.05, label='tau='+str(5-i),color=colors[i])
+
+plt.legend()
+plt.xlabel('Iteration')
+plt.ylabel('Accuracy')
+plt.title('Accuracy vs Iteration')
+plt.grid()
+plt.show()
+plt.figure(2)
+
+for i in range(5):
+    plt.plot(1-np.abs(obj_t[i]-181.696)/181.696+0.05, label='tau='+str(5-i), color= colors[i])
+
+plt.legend()
+plt.xlabel('time step') 
+plt.ylabel('Accuracy')
+plt.title('Accuracy vs Time')
+plt.grid()
+plt.show()
+
+
+obj_t_N = [np.load('obj_t_N2g_t46.69468853115465.npy'),\
+            np.load('obj_t_N4g_t41.97323331390489.npy'),\
+            np.load('obj_t_N8g_t37.54527775471204.npy'),\
+            np.load('obj_t_N16g_t37.4153355353034.npy')]
+opt = np.array([46.69, 41.97323331390489, 37.54527775471204, 37.4153355353034])
+plt.figure(3)
+for i in range(4):
+    plt.plot(1-np.abs(obj_t_N[i]-opt[i])/opt[i], label='N='+str(2**(i+1)))
+plt.legend()
+plt.xlabel('time step')
+plt.ylabel('Accuracy')
+plt.title('Accuracy vs Time')
+plt.grid()
+plt.show()
+        
