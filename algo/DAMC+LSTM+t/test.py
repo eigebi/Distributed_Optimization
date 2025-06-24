d383d9c574b00ab4286c93554214553660ba0852
Shuo Chen <1528674004@qq.com>
2025-06-24 19:02:19 +0100

    tbc

--- algo/DAMC+LSTM+t/test.py
+++ algo/DAMC+LSTM+t/test.py
@@ -39,16 +39,16 @@ def derive_grad_lambda_with_x(x, r):
 if __name__ == "__main__":
     obj = [lambda x: x**2, lambda x: (x-5)**2]
     jac = [lambda x: 2*x, lambda x: 2*(x-5)]
-    obj = [lambda x: -1/(1+np.exp(-2*(x-3))), lambda x: -1/(1+np.exp(-1*(x-10)))]
-    jac = [lambda x: obj[0](x)*(1-obj[0](x)), lambda x: obj[1](x)*(1-obj[1](x))]
-    jac = [lambda x:-1/(x+1)/np.log(1+1),lambda x:-1/(x+1)/np.log(5+1)]
+    #obj = [lambda x: 1/(1+np.exp(-2*(x-3))), lambda x: 1/(1+np.exp(-1*(x-10)))]
+    #jac = [lambda x: -obj[0](x)*(1-obj[0](x)), lambda x: -obj[1](x)*(1-obj[1](x))]
+    #jac = [lambda x:-1/(x+1)/np.log(1+1),lambda x:-1/(x+1)/np.log(5+1)]
     # add constraints x1+x2 < 1, this constraint is assigned to agent 1
     r = np.zeros(5, dtype=np.float32)  # Initialize r (lambda)
-    z = np.array([0, 0], dtype=np.float32)  # Example z(k) values
+    z = np.array([0.5, 0.5], dtype=np.float32)  # Example z(k) values
     len_x = [1,2]
     gamma = [np.zeros([i], dtype=np.float32) for i in len_x]  # Initialize gamma
-    rho = 10
-    beta = 0
+    rho = 15
+    beta = 1500
     class arg_nn:
         hidden_size = 32
         hidden_size_x = 20
@@ -67,9 +67,10 @@ if __name__ == "__main__":
     x = [torch.randn(1,1), torch.randn(1,2)]  # Initialize x for each model
     num_layer = 2
     num_problem = 1
-    for k in range(10):
+    for k in range(20):
         r = np.zeros(5, dtype=np.float32)  # Initialize r (lambda)
-        z = np.array([0, 0], dtype=np.float32)  # Example z(k) values
+        reserved_r = r
+        z = np.array([1.5, 0.5], dtype=np.float32)  # Example z(k) values
         gamma = [np.zeros([i], dtype=np.float32) for i in len_x]  # Initialize gamma
         x = [torch.randn(1,1), torch.randn(1,2)]
 
@@ -77,30 +78,11 @@ if __name__ == "__main__":
         hidden_lambda = (torch.randn(num_layer, num_problem, arg_nn.hidden_size), torch.randn(num_layer, num_problem, arg_nn.hidden_size)) # (num_layer, batch_size, hidden_size)
 
 
-        for f in range(500):
-            for j in range(10):
-                if not LSTM_lambda:
-                    # update lambda using gradient ascent
-                
-                    r = np.maximum(r + 0.1 * (np.concatenate([-z,z-6,np.sum(z, keepdims=True)-1], axis=0)), 0)
-                else:
-                # alternatively, using LSTM
-                    r = reserved_r
-                    grad_lambda = 10*derive_grad_lambda(z, r)
-                    #grad_lambda = derive_grad_lambda_with_x(x,r)
-                    delta_r, hidden_lambda_temp = lambda_model(torch.tensor(grad_lambda[np.newaxis,:], dtype=torch.float32), hidden_lambda)
-                    hidden_lambda = (hidden_lambda_temp[0].detach(), hidden_lambda_temp[1].detach())
-                    _r = torch.tensor(r,dtype=torch.float32) + delta_r[0,0]
-                    r = _r.detach().numpy()
-                    grad_lambda = 10*derive_grad_lambda(z, r)
-                    #grad_lambda = derive_grad_lambda_with_x(x,r)
-                    _r.unsqueeze(0).backward(-torch.tensor(grad_lambda[np.newaxis,:],dtype=torch.float32), retain_graph=True)
-                    reserved_r = r
-                    r = lambda_proj(r)
-                
-
+        for f in range(1000):
+            for j in range(20):
                 
                 grad_x = jac[0](x[0]) + (-r[0]+r[2] + gamma[0] + rho * (x[0].numpy() - z[0]))
+                #grad_x = torch.tensor(jac[0](z[0]) + (-r[0]+r[2] + gamma[0] + rho * (x[0].numpy() - z[0])), dtype=torch.float32)
                 delta, _ = x_models[0](grad_x)
                 _x = x[0] + delta[0]
                 x[0] = _x.detach()
@@ -126,17 +108,44 @@ if __name__ == "__main__":
                 grad_x[:,0] = torch.tensor(r[4] + gamma[1][0] + rho * (x[1][:,0].numpy() - z[0]))
                 _x.backward(grad_x, retain_graph=True)
                 gamma[1] = gamma[1] + rho * (x[1].numpy() - z)[0]
-
                 z[0] = (gamma[0] + rho * x[0].numpy() + gamma[1][0] + rho * x[1][:,0].numpy()+ beta * z[0])[0,0] / (2 * rho + beta)
                 z[1] = (gamma[1][1] + rho * x[1][:,1].numpy() + beta * z[1])[0] / (rho + beta)
                 #z = np.ones(2)
-                pass
-
+            
+                
             for i in range(2):
                 x_optimizers[i].step()
                 x_optimizers[i].zero_grad()
-            lambda_optimizer.step()
-            lambda_optimizer.zero_grad()
+            #gamma[0] = gamma[0] + rho * (x[0].numpy() - z[0])[0]
+            #gamma[1] = gamma[1] + rho * (x[1].numpy() - z)[0]
+            
+            
+
+            # update lambda, every n iterations (when x update is stable)
+            
+            if not LSTM_lambda:
+                # update lambda using gradient ascent
+            
+                r = np.maximum(r + 0.1 * (np.concatenate([-z,z-6,np.sum(z, keepdims=True)-1], axis=0)), 0)
+            else:
+            # alternatively, using LSTM
+                r = reserved_r
+                grad_lambda = derive_grad_lambda(z, r)
+                #grad_lambda = derive_grad_lambda_with_x(x,r)
+                delta_r, hidden_lambda_temp = lambda_model(torch.tensor(grad_lambda[np.newaxis,:], dtype=torch.float32), hidden_lambda)
+                hidden_lambda = (hidden_lambda_temp[0].detach(), hidden_lambda_temp[1].detach())
+                _r = torch.tensor(r,dtype=torch.float32) + delta_r[0,0]
+                r = _r.detach().numpy()
+                grad_lambda = derive_grad_lambda(z, r)
+                #grad_lambda = derive_grad_lambda_with_x(x,r)
+                _r.unsqueeze(0).backward(-torch.tensor(grad_lambda[np.newaxis,:],dtype=torch.float32), retain_graph=True)
+                reserved_r = r
+                r = lambda_proj(r)
+            
+            if f % 10 == 0:
+                lambda_optimizer.step()
+                lambda_optimizer.zero_grad()
+
             print("x:",x,"lambda:",r)
             print("z:", z," gamma:", gamma)
             #print("obj: ", obj[0](x[0].numpy())+obj[1](x[1][:,1].numpy()))
@@ -156,7 +165,7 @@ if __name__ == "__main__":
                 if not LSTM_lambda:
                     # update lambda using gradient ascent
                 
-                    r = np.maximum(r + 0.1 * (np.concatenate([-z,z-6,np.sum(z, keepdims=True)-1], axis=0)), 0)
+                    r = np.maximum(r + 0.5 * (np.concatenate([-z,z-6,np.sum(z, keepdims=True)-1], axis=0)), 0)
                 else:
                 # alternatively, using LSTM
                     r = reserved_r
